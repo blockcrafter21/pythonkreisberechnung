805ef48a6101fa6f91f0b0b2a9342c2a02acd8c2
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2020-12-11 17:45:35 +0100

    v1

---
+++ main.py
@@ -0,0 +1,27 @@
+import math
+
+
+seitenlänge1=0
+seitenlänge2=0
+
+#hauptteil
+seitenlänge1= input("Bitte die eine seitenlänge eigeben")
+seitenlänge1=float(seitenlänge1)
+seitenlänge2= input("Bitte die andere seitenlänge eingeben")
+seitenlänge2=float(seitenlänge2)
+print(seitenlänge1)
+print(seitenlänge2)
+if seitenlänge1 < seitenlänge2:
+    durchmesser=seitenlänge1
+    radius = durchmesser / 2
+    flächedeskreises = math.pi * durchmesser
+    print(f"die fläche des kreises ist{flächedeskreises}")
+
+
+
+else:
+    durchmesser=seitenlänge2
+    radius = durchmesser / 2
+    flächedeskreises = math.pi * radius**2
+    print(f"die fläche des kreises ist{flächedeskreises}")
+
