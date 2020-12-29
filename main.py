a9fac92542e040863833228a7f45e2e7bffa8ba2
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2020-12-29 08:59:29 +0100

    v1.3
    some changes
    stay healthy

--- main.py
+++ main.py
@@ -7,24 +7,26 @@ seitenlaenge1 = 0
 seitenlaenge2 = 0
 
 print('Blockcrafter21,email:blockdevelopment@gmx.net, all rights reserved')
+start = input('Press 5 to start')
 
-# hauptteil
-seitenlaenge1 = input("Bitte die eine Seitenlaenge eingeben")
-seitenlaenge1 = float(seitenlaenge1)
-seitenlaenge2 = input("Bitte die andere Seitenlaenge eingeben")
-seitenlaenge2 = float(seitenlaenge2)
-print(seitenlaenge1)
-print(seitenlaenge2)
-if seitenlaenge1 < seitenlaenge2:
-    durchmesser = seitenlaenge1
-    radius = durchmesser / 2
-    flaechedeskreises = math.pi * durchmesser
-    print(f"die Fläche des Kreises ist{flaechedeskreises}")
+if start == 5:
+    # hauptteil
+    seitenlaenge1 = input("Bitte die eine Seitenlaenge eingeben")
+    seitenlaenge1 = float(seitenlaenge1)
+    seitenlaenge2 = input("Bitte die andere Seitenlaenge eingeben")
+    seitenlaenge2 = float(seitenlaenge2)
+    print(seitenlaenge1)
+    print(seitenlaenge2)
+    if seitenlaenge1 < seitenlaenge2:
+        durchmesser = seitenlaenge1
+        radius = durchmesser / 2
+        flaechedeskreises = math.pi * durchmesser
+        print(f"die Fläche des Kreises ist{flaechedeskreises}")
 
+    else:
+        durchmesser = seitenlänge2
+        radius = durchmesser / 2
+        flaechedeskreises = math.pi * radius ** 2
+        print(f"die fläche des kreises ist{flaechedeskreises}")
 else:
-    durchmesser = seitenlänge2
-    radius = durchmesser / 2
-    flaechedeskreises = math.pi * radius ** 2
-    print(f"die fläche des kreises ist{flaechedeskreises}")
-
-
+    print('hey wait')
