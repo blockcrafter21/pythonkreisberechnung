2e87cf7229075963d5f6a5d36d3c67f6af0b8e16
blockcrafter21 <72599189+blockcrafter21@users.noreply.github.com>
2020-12-29 09:13:45 +0100

    v.1.5
    some important changes

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ sidelength2= 0
 
 print('Blockcrafter21,email:blockdevelopment@gmx.net, all rights reserved')
 start = input('Press 5 to start')
+start = float(start)
 
 if start == 5:
     # hauptteil
-    sidelength1 = input("Bitte die eine Seitenlaenge eingeben")
-    sidelength1 = float(seitenlaenge1)
-    sidelength2 = input("Bitte die andere Seitenlaenge eingeben")
-    sidelength2 = float(seitenlaenge2)
+    sidelength1 = input("Please enter the one side length")
+    sidelength1 = float(sidelength1)
+    sidelength2 = input("Please enter the other side length")
+    sidelength2 = float(sidelength2)
     print(sidelength1)
     print(sidelength2)
     if sidelength1 < sidelength2:
         diameter = sidelength1
         radius = diameter / 2
         circulararea = math.pi * diameter
-        print(f"die Fläche des Kreises ist{circulararea}")
+        print(f"{circulararea}is the area of the circle")
 
     else:
         diameter = sidelength2
         radius = diameter / 2
         circulararea = math.pi * radius ** 2
-        print(f"die fläche des kreises ist{circulararea}")
+        print(f"{circulararea}is the area of the circle")
 else:
-    print('hey wait')
+    print('hey wait wrong number ')
